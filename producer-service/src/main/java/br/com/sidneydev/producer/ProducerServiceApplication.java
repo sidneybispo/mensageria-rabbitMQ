b7abedb832b19d5b8e5e889733cbb5aedbc86910
sidneybispo <protecney@gmail.com>
2023-12-20 16:37:46 -0300

    start producer service

---
+++ producer-service/src/main/java/br/com/sidneydev/producer/ProducerServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.sidneydev.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerServiceApplication.class, args);
+    }
+
+}
