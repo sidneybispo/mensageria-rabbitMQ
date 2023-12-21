e67c0080f55c81ecf924f602182686ab8c1806d9
sidneybispo <protecney@gmail.com>
2023-12-21 11:46:25 -0300

    start consumer string service

---
+++ consumer/src/main/java/br/com/sidneydev/consumerservice/rabbitmq/consumers/StringConsumer.java
@@ -0,0 +1,18 @@
+package br.com.sidneydev.consumerservice.rabbitmq.consumers;
+
+
+
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+public class StringConsumer {
+
+    @RabbitListener(queues = {"product.log"})
+    public void consumer(String message) {
+        log.info("Consumer received a message " + message);
+    }
+}
